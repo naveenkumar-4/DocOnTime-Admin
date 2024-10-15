16c8ebbf6bb814fa0c3bdc1adadaf015fb8c1ffc
Naveen Kumar <naveenkumarrockz362@gmail.com>
2024-10-15 15:17:54 +0530

    DocOnTime Admin

---
+++ src/context/AppContext.jsx
@@ -0,0 +1,36 @@
+import { createContext } from "react";
+
+export const AppContext = createContext();
+
+
+
+const AppContextProvider = (props) => {
+
+    const currency = '$';
+
+    const months = ["", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    const slotDateFormate = (slotDate) => {
+        const dateArray = slotDate.split("_");
+        return dateArray[0] + " " + months[Number(dateArray[1])] + " " + dateArray[2];
+    }
+
+
+    const calculateAge = (dob) => {
+        const today = new Date();
+        const birthDate = new Date(dob)
+
+        let age = today.getFullYear() - birthDate.getFullYear()
+
+        return age
+    }
+
+    const value = { calculateAge, slotDateFormate, currency }
+
+    return (
+        <AppContext.Provider value={value}>
+            {props.children}
+        </AppContext.Provider>
+    )
+}
+
+export default AppContextProvider
---
+++ src/pages/Doctor/DoctorDashboard.jsx
@@ -0,0 +1,88 @@
+import React, { useContext, useEffect } from 'react'
+import { DoctorContext } from '../../context/DoctorContext'
+import { assets } from '../../assets/assets_admin/assets'
+import { AppContext } from '../../context/AppContext'
+import { AdminContext } from '../../context/AdminContext'
+
+const DoctorDashboard = () => {
+
+  const { dToken, dashData, getDashData, cancelAppointment, completeAppointment } = useContext(DoctorContext)
+  const { currency, slotDateFormate } = useContext(AppContext)
+  const {getAllAppointments} = useContext(AdminContext)
+
+  useEffect(() => {
+    getDashData()
+  }, [dToken])
+
+  return dashData && (
+    <div className='m-5'>
+      <div className='flex flex-wrap gap-3'>
+
+        <div className='flex items-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-gray-100 cursor-pointer hover:scale-105 transition-all'>
+          <img className='w-14' src={assets.earning_icon} alt="" />
+          <div>
+            <p className='text-xl font-semibold text-gray-600'>{currency} {dashData.earnings}</p>
+            <p className='text-gray-400'>Earnings</p>
+          </div>
+        </div>
+
+        <div className='flex items-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-gray-100 cursor-pointer hover:scale-105 transition-all'>
+          <img className='w-14' src={assets.appointments_icon} alt="" />
+          <div>
+            <p className='text-xl font-semibold text-gray-600'>{dashData.appointments}</p>
+            <p className='text-gray-400'>Appointments</p>
+          </div>
+        </div>
+
+
+        <div className='flex items-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-gray-100 cursor-pointer hover:scale-105 transition-all'>
+          <img className='w-14' src={assets.patients_icon} alt="" />
+          <div>
+            <p className='text-xl font-semibold text-gray-600'>{dashData.patients}</p>
+            <p className='text-gray-400'>Patients</p>
+          </div>
+        </div>
+
+
+      </div>
+
+      <div className='bg-white'>
+
+        <div className='flex items-center gap-2.5 px-4 py-4 mt-10 rounded-t border'>
+          <img src={assets.list_icon} alt="" />
+          <p className='font-semibold'>Latest Bookings</p>
+        </div>
+
+        <div className='pt-4 border border-t-0'>
+
+          {
+            dashData.latestAppointments.map((appointment, index) => (
+              <div className='flex items-center px-6 py-3 gap-3 hover:bg-gray-100' key={index}>
+                <img className='rounded-full w-10' src={appointment.userData.image} alt="" />
+                <div className='flex-1 text-sm'>
+                  <p className='text-gray-800 font-medium'>{appointment.userData.name}</p>
+                  <p className='text-gray-600 '>{slotDateFormate(appointment.slotDate)}</p>
+                </div>
+                {
+                  appointment.cancelled
+                    ? <p className='text-red-400 text-xs font-medium'>Cancelled</p>
+                    : appointment.isCompleted
+                      ? <p className='text-green-400 text-xs font-medium'>Completed</p>
+                      :
+                      <div className='flex'>
+                        <img onClick={() => cancelAppointment(appointment._id)} className='w-10 cursor-pointer' src={assets.cancel_icon} alt="" />
+                        <img onClick={() => completeAppointment(appointment._id)} className='w-10 cursor-pointer' src={assets.tick_icon} alt="" />
+                      </div>
+                }
+              </div>
+            ))
+          }
+
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default DoctorDashboard
